4a9e221091fa407f86798cd811d6db26e9c63417
Pis2la <gunnyf4@gmail.com>
2024-08-19 10:24:12 +0700

    refsfunc01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
 import Calendar from './class-components'
-import { Header,Content,Footer } from './func-components'
+import RefsFunc from './refs-func'
 export default function app(){
   return(
-    <>
-      <Header/>
-      <Content/>
-      <Footer/>
-      <p><center><Calendar/></center></p>
-    </>
+    <RefsFunc/>
   )
 }
